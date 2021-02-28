391ab49bf73e7a98268d7ef657e77aa2bcbde753
aanjan <aanjanbhuwan@gmail.com>
2021-02-28 21:19:50 +0545

    Response Formater added for android

---
+++ android/src/main/java/com/reactnativeimepay/ResponseFormate.java
@@ -0,0 +1,6 @@
+package com.reactnativeimepay;
+
+class ResponseFormate {
+  public ResponseFormate(int responseCode, String responseDescription, String transactionId, String msisdn, String amount, String refId) {
+  }
+}
